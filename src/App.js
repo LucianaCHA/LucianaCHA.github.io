e092dc43eba3604647f4286edcef2eaf3efd8eec
Luciana Chamorro <lucianachamorro87@gmail.com>
2022-10-18 22:35:25 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
+
+document.documentElement.style.setProperty('--main-bg-color', randomColor);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div id="quote-box">
+        <div id="text">
+          <p>Random Quote</p>
+        </div>        
+      </div>
     </div>
   );
 }
