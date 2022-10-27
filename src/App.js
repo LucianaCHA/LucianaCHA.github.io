ce36594770e504a6cc483ecb7b22b349a0def0d0
Luciana Chamorro <lucianachamorro87@gmail.com>
2022-10-26 22:09:56 -0300

    Ajuste de estilo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import './App.css';
-
+import { quotesMock } from './quotesMock';
 function App() {
 
-  const newQuote = () => {
+const newQuote = () => {
 
     const randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
     const color = "#"+((1<<6)*Math.random()|0).toString(16); 
-
     
     document.documentElement.style.setProperty('--main-bg-color', randomColor);
     document.documentElement.style.setProperty('--main-color', color);
-  }
+    
+  const randomQuote = quotesMock[Math.floor(Math.random() * quotesMock.length)];
 
+  let text = randomQuote.text;
+  let author = randomQuote.author;
+  console.log(randomQuote);
+  document.getElementById('text').innerHTML = text;
   
+  document.getElementById('author').innerHTML = author;
+}
 
   return (
     <div className="App" >
       <div id="quote-box">
         <div id="text">
-          <p>Random Quote</p>
+          <p>{}</p>
         </div>
         <div id="author">
-          <p>Random Author</p>
+          <p>{}</p>
         </div>
         <div id="new-quote">
           <button onClick= {newQuote} color="main" className='new-quote'> New quote</button>
-        </div> 
         <div id="tweet">
           <a href="twitter.com/intent/tweet" id="tweet-quote">Tweet quote</a>
         </div>
+        </div> 
 
       </div>
     </div>
